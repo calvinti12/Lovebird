8266611ba56ad2cd2610666e97f306c12fbd8ab3
Ryan <rylee@college.harvard.edu>
2016-10-22 15:34:07 -0400

    facebook

--- app/bot/functions.rb
+++ app/bot/functions.rb
@@ -34,8 +34,8 @@ def create_user(message)
 end
 
 def create_relationship(user_id, crush_first_name, crush_last_name)
-	if Relationship.find_by(facebook_id: user_id)
-		Relationship.find_by(facebook_id: user_id).destroy
+	if Relationship.find_by(user_id: user_id)
+		Relationship.find_by(user_id: user_id).destroy
 	end
 
 	users = User.where(first_name: crush_first_name, last_name: crush_last_name)
@@ -62,7 +62,7 @@ def create_relationship(user_id, crush_first_name, crush_last_name)
 end
 
 def check_match(user_id, crush_id)
-	if Relationship.find_by(facebook_id: user_id).pluck(:crush_id) == Relationship.find_by(facebook_id: crush_id).pluck(:crush_id)
+	if Relationship.find_by(user_id: user_id).pluck(:crush_id) == Relationship.find_by(user_id: crush_id).pluck(:crush_id)
 		return true
 	else
 		return false
